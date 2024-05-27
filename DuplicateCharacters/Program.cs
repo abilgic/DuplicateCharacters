9b3fa091b207a1d02d7e12918ec127a927bebc74
ahmet bilgic <ahmet_bilgic@yahoo.com>
2024-05-28 01:30:59 +0300

    Add project files.

---
+++ DuplicateCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿string str = "happy";
+
+var dict = new Dictionary<char, int>();
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (dict.ContainsKey(str[i]))
+    {
+        dict[str[i]]++;
+    }
+    else
+    {
+        dict[str[i]] = 1;
+    }
+}
+
+foreach (var item in dict)
+{
+    if (item.Value >= 2)
+    {
+        Console.Write(item.Key);
+    }
+}
